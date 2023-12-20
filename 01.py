af5f037254ffd333a19f18284cbc838d0af357b1
Коля <Shekezzz@gmail.com>
2023-12-20 20:54:56 +0200

    add new code

--- 01.py
+++ 01.py
@@ -1,13 +1,8 @@
 sentence = input("Введіть речення з двох слів: ")
 
-word1, word2 = sentence.split()
 
-formatted1 = f"{word2.upper()} {word1.capitalize()}"
-formatted2 = "{} {}".format(word2.title(), word1.upper())
-formatted3 = "{1} {0}".format(word1.capitalize(), word2.title())
 
-print(formatted1, formatted2, formatted3, sep = "<<<>>>")
-
+print(sentence)
 
 
 
